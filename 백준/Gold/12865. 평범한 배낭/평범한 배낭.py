5d8134164b5ceec01524102ac82447417ce32627
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-03-15 23:14:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 3000 ms, Memory: 229084 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,31 +1,21 @@
-def knapsack_max_value(N, K, weights, values):
-    # DP 테이블 초기화
+def knapsack(N, K, weights, values):
     dp = [[0] * (K + 1) for _ in range(N + 1)]
-
-    # 물건을 하나씩 고려하여 DP 테이블 채우기
+    
     for i in range(1, N + 1):
-        for w in range(1, K + 1):
-            # 현재 물건을 배낭에 넣을 수 있는 경우
-            if weights[i - 1] <= w:
-                # 현재 물건을 선택하는 경우와 선택하지 않는 경우 중 큰 가치 선택
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+        for j in range(1, K + 1):
+            if weights[i - 1] <= j:
+                dp[i][j] = max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]])
             else:
-                # 현재 물건을 배낭에 넣을 수 없는 경우
-                dp[i][w] = dp[i - 1][w]
-
-    # 최적해의 가치 반환
+                dp[i][j] = dp[i - 1][j]
+    
     return dp[N][K]
 
-# 입력 받기
 N, K = map(int, input().split())
 weights = []
 values = []
-
 for _ in range(N):
-    W, V = map(int, input().split())
-    weights.append(W)
-    values.append(V)
+    w, v = map(int, input().split())
+    weights.append(w)
+    values.append(v)
 
-# 최대 가치 계산 및 출력
-result = knapsack_max_value(N, K, weights, values)
-print(result)
+print(knapsack(N, K, weights, values))
