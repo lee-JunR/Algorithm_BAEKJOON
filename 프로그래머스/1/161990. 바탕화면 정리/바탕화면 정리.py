ee60c5e9879bba59b657c28af3264cf5509eaa3a
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-03-28 22:29:55 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -0,0 +1,18 @@
+def solution(wallpaper):
+    leftmost_x = len(wallpaper[0])
+    leftmost_y = len(wallpaper)
+    rightmost_x = -1
+    rightmost_y = -1
+
+    # 바탕화면을 순회하면서 파일의 위치를 찾음
+    for y in range(len(wallpaper)):
+        for x in range(len(wallpaper[y])):
+            if wallpaper[y][x] == '#':
+                # 가장 왼쪽 위의 파일 좌표 찾기
+                leftmost_x = min(leftmost_x, x)
+                leftmost_y = min(leftmost_y, y)
+                # 가장 오른쪽 아래의 파일 좌표 찾기
+                rightmost_x = max(rightmost_x, x)
+                rightmost_y = max(rightmost_y, y)
+
+    return [leftmost_y, leftmost_x, rightmost_y+1, rightmost_x+1]
