13fbffa1a3c901fdbf44b91d7d426d76a36e9dd6
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-04-04 00:08:25 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.94 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,25 +1,21 @@
-import math
+def count_divisors(number):
+    divisors_count = [0] * (number + 1)
+
+    for i in range(1, number + 1):
+        for j in range(i, number + 1, i):
+            divisors_count[j] += 1
+
+    return divisors_count
+
 
 def solution(number, limit, power):
+    divisors_count = count_divisors(number)
     answer = 0
-    divisors = count_divisors(number)
 
-    for i in divisors:
-        if i > limit:
+    for count in divisors_count[1:number + 1]:
+        if count > limit:
             answer += power
         else:
-            answer += i
+            answer += count
+
     return answer
-def count_divisors(number):
-    divisors_list = [-1] + [1] + [0] * 99_999  # -1은 index를 위해 버리는 수
-    
-    for i in range(2, number + 1):
-        root_i = int(math.sqrt(i))
-        for j in range(1, root_i + 1):
-            if i % j == 0:
-                divisors_list[i] += 1
-        if root_i * root_i == i:
-            divisors_list[i] = divisors_list[i] * 2 - 1
-        else:
-            divisors_list[i] = divisors_list[i] * 2
-    return divisors_list[1:number + 1]
