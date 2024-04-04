85e3494c683deb10bc3124d27c606e413312f821
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-04-04 21:49:30 +0900

    [level 2] Title: 할인 행사, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -7,14 +7,23 @@ def list_to_dict(lst):
             dct[i] = 1
     return dct
 
-
 def solution(want, number, discount):
     answer = 0
     wishlist = dict(zip(want, number))
     total_count = sum(number)
+    discount_dict = list_to_dict(discount[:total_count])
 
     for i in range(len(discount) - total_count + 1):
-        if list_to_dict(discount[i:total_count + i]) == wishlist:
+        if discount_dict == wishlist:
             answer += 1
+        # 할인 목록을 한 칸씩 옮겨가면서 사전을 업데이트
+        if i + total_count < len(discount):
+            discount_dict[discount[i]] -= 1
+            if discount_dict[discount[i]] == 0:
+                del discount_dict[discount[i]]
+            if discount[i + total_count] in discount_dict:
+                discount_dict[discount[i + total_count]] += 1
+            else:
+                discount_dict[discount[i + total_count]] = 1
 
     return answer
