151372fbfd1615e0028e5b8eb5eb8c277061553f
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-04-09 04:57:25 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -1,9 +1,22 @@
 def solution(numbers):
-    answer = []
-    for i in range(len(numbers)):
-        for j in range(i + 1, len(numbers)):  # i 이후의 인덱스만 확인
-            sum_num = numbers[i] + numbers[j]
-            if sum_num not in answer:  # 중복 확인
-                answer.append(sum_num)
-    answer.sort()  # 결과 정렬
-    return answer
+    memo = {}  # 메모이제이션을 위한 딕셔너리
+    
+    def calculate_sum(i, j):
+        # 이미 계산한 값이 있는 경우 메모된 값을 반환
+        if (i, j) in memo:
+            return memo[(i, j)]
+        
+        # 두 수의 합을 계산하고 메모에 저장
+        memo[(i, j)] = numbers[i] + numbers[j]
+        return memo[(i, j)]
+    
+    result_set = set()
+    n = len(numbers)
+    
+    for i in range(n):
+        for j in range(i+1, n):
+            result_set.add(calculate_sum(i, j))
+    
+    result = sorted(result_set)
+    
+    return result
