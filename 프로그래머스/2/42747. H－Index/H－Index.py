ce42f9a7ca9b52cde686cb92acc69f2b7f0ff49c
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-04-22 22:42:31 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,12 +1,8 @@
 def solution(citations):
-    answer = 0
-
-    citations.sort()
-    h_index = [0] * len(citations)
-    for i, citation in enumerate(citations):
-        n = sum([1 if citation <= c else 0 for c in citations[i:]])
-        h_index[i] = min(citation, n)
-    answer = max(h_index)
-    return answer
-
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
 
