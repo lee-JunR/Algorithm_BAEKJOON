630d6d6eca5253f804fbe1565f2348d8bc41d3a4
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-04-30 17:19:39 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,26 +1,37 @@
 from collections import deque
 import sys
+
+
+
+
+def shortest_path(maze, n, m):
+    # 방향 이동을 위한 배열: 상하좌우
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    # 방문 기록을 위한 배열
+    visited = [[False for _ in range(m)] for _ in range(n)]
+    # BFS를 위한 큐
+    queue = deque([(0, 0, 1)])  # 시작 위치 (row, column, distance)
+    visited[0][0] = True
+
+    while queue:
+        row, col, distance = queue.popleft()
+        # 도착 위치에 도달한 경우
+        if row == n - 1 and col == m - 1:
+            return distance
+
+        # 4방향으로 이동
+        for dr, dc in directions:
+            new_row = row + dr
+            new_col = col + dc
+            # 미로의 범위를 벗어나지 않고, 이동할 수 있는 칸이며, 아직 방문하지 않은 경우
+            if 0 <= new_row < n and 0 <= new_col < m and maze[new_row][new_col] == 1 and not visited[new_row][new_col]:
+                queue.append((new_row, new_col, distance + 1))
+                visited[new_row][new_col] = True
+
+
+# 입력 예시
 input = sys.stdin.readline
 N, M = map(int, input().split())
+maze = [list(map(int, ' '.join(input().split()))) for _ in range(N)]
 
-# 2차원 리스트 생성
-graph = [list(map(int, ' '.join(input().split()))) for _ in range(N)]
-
-queue = deque([(0, 0)])
-
-# 미로 문제 풀 때는 이동을 표현해준다.
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-cnt = 0
-
-# BFS
-while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-        next_x, next_y = x+dx[i], y+dy[i]
-        if 0 <= next_x < N and 0 <= next_y < M:  # 범위 확인
-            if graph[next_x][next_y] == 1:  # 경로 확인
-                queue.append((next_x, next_y))
-                graph[next_x][next_y] = graph[x][y] + 1  # value 자체를 이동 횟수로 사용
-
-print(graph[N-1][M-1])
+print(shortest_path(maze, N, M))
