72e9275ee3a45787e789fbcc4ec2af2a6a7b4213
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-05-05 23:03:46 +0900

    [Silver I] Title: 동물원, Time: 60 ms, Memory: 34972 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,21 +1,9 @@
-import sys
+def solution():
+    n = int(input())
+    dp = [3, 7]
+    for i in range(1, n):
+        dp.append((dp[i - 1] + dp[i] * 2) % 9901)
+    print(dp[n - 1])
 
-input = sys.stdin.readline
 
-n = int(input())
-
-# dp 배열 초기화
-dp = [[0, 0, 0] for _ in range(n + 1)]
-
-# 첫 번째 열의 기본 배치
-dp[1] = [1, 1, 1]  # 초기 상태는 세 가지 모두 가능
-
-for i in range(2, n + 1):
-    dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901
-    dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901
-    dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % 9901
-
-result = (dp[n][0] + dp[n][1] + dp[n][2]) % 9901
-
-# 결과 출력
-print(result)
+solution()
