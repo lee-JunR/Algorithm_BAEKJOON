468c78bf0fc25cc890f3fa1b807b05b6b91dd373
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-05-13 23:43:24 +0900

    [Bronze V] Title: X보다 작은 수, Time: 176 ms, Memory: 15300 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,30 +1,34 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws IOException {
+    // BufferedReader를 사용
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int n = sc.nextInt();
-    int x = sc.nextInt();
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+    int x = Integer.parseInt(st.nextToken());
+
     int[] A = new int[n];
 
+    // 두 번째 줄에서 배열 A의 요소들을 입력받고, 동시에 x보다 작은 요소를 한번에 처리
+    st = new StringTokenizer(br.readLine());
+    StringBuilder result = new StringBuilder();
     for (int i = 0; i < n; i++) {
-      A[i] = sc.nextInt();
-    }
-    sc.close();
-
-//    확인
-//    System.out.println(n);
-//    System.out.println(x);
-//    System.out.println(Arrays.toString(A));
-
-    for (int i = 0; i < n; i++) {
+      A[i] = Integer.parseInt(st.nextToken());
       if (A[i] < x) {
-        System.out.print(A[i] + " ");
+        // x보다 작은 요소들을 StringBuilder에 추가
+        result.append(A[i]).append(" ");
       }
     }
-  }
 
+    System.out.println(result.toString());
+
+    br.close();
+  }
 }
