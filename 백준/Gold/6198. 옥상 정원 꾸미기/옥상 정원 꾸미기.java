91d9cb29a02725ac5cbcca9bd14f8963691321dd
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-06-04 10:05:18 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 288 ms, Memory: 24072 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.java
@@ -1,16 +1,14 @@
-// 옥상 정원 꾸미기 6198
-// 빌딩의 높이를 가진 자료가 1_000_000_000 개이므로 O(n)의 시간복잡도를 떠올리자.
-
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+    Stack<Integer> stack = new Stack<>();
+    long result = 0;
     //input
     int n = Integer.parseInt(br.readLine());
     int[] heightsOfBuildings = new int[n];
@@ -18,21 +16,18 @@ public class Main {
       heightsOfBuildings[i] = Integer.parseInt(br.readLine());
     }
 
-    long totalViewCount = 0;
-    Stack<Integer> stack = new Stack<>();
-
     for (int i = 0; i < n; i++) {
-      // 현재 빌딩보다 낮은 빌딩을 스택에서 제거
-      while (!stack.isEmpty() && stack.peek() <= heightsOfBuildings[i]) {
+      while (!stack.isEmpty() && heightsOfBuildings[i] >= stack.peek()) {
         stack.pop();
       }
-      // 스택에 남아있는 빌딩은 현재 빌딩에서 볼 수 있는 빌딩
-      totalViewCount += stack.size();
-      // 현재 빌딩을 스택에 추가
+      result += stack.size();
       stack.push(heightsOfBuildings[i]);
     }
 
-    // Output
-    System.out.println(totalViewCount);
+    //output
+    System.out.println(result);
+
+
+    br.close();
   }
 }
