5067e2e022c30b336d65931a6040a5f99768ef55
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-06-18 22:49:56 +0900

    [Silver IV] Title: 덱, Time: 188 ms, Memory: 18832 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,116 +1,63 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
-class ArrDeque {
-  private int[] deque;
-  private int head;
-  private int tail;
-  private int capacity;
-
-  public ArrDeque(int capacity) {
-    this.capacity = capacity;
-    deque = new int[capacity];
-    head = capacity / 2;
-    tail = capacity / 2;
-  }
-
-  public void push_front(int data) {
-    if (head == 0) {
-      System.out.println("Deque is full at the front");
-      return;
-    }
-    deque[--head] = data;
-  }
-
-  public void push_back(int data) {
-    if (tail == capacity) {
-      System.out.println("Deque is full at the back");
-      return;
-    }
-    deque[tail++] = data;
-  }
-
-  public int pop_front() {
-    if (isEmpty()) {
-      return -1;
-    }
-    return deque[head++];
-  }
-
-  public int pop_back() {
-    if (isEmpty()) {
-      return -1;
-    }
-    return deque[--tail];
-  }
-
-  public int front() {
-    if (isEmpty()) {
-      return -1;
-    }
-    return deque[head];
-  }
-
-  public int back() {
-    if (isEmpty()) {
-      return -1;
-    }
-    return deque[tail - 1];
-  }
-
-  public int size() {
-    return tail - head;
-  }
-
-  public int empty() {
-    return isEmpty() ? 1 : 0;
-  }
-
-  private boolean isEmpty() {
-    return tail == head;
-  }
-}
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringBuilder sb = new StringBuilder();
-    int n = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
 
-    ArrDeque deque = new ArrDeque(10000); // 명령의 수 N외 최댓값으로 가정
+        Deque<String> deque = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) {
+            String[] command = br.readLine().split(" ");
 
-    for (int i = 0; i < n; i++) {
-      String[] command = br.readLine().split(" ");
+            switch (command[0]) {
+                case "push_front":
+                    deque.addFirst(command[1]);
+                    break;
+                case "push_back":
+                    deque.addLast(command[1]);
+                    break;
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.removeFirst()).append("\n");
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.removeLast()).append("\n");
+                    }
+                    break;
+                case "size":
+                    sb.append(deque.size()).append("\n");
+                    break;
+                case "empty":
+                    sb.append(deque.isEmpty() ? "1\n" : "0\n");
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.peekFirst()).append("\n");
+                    }
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        sb.append("-1\n");
+                    } else {
+                        sb.append(deque.peekLast()).append("\n");
+                    }
+                    break;
+            }
+        }
 
-      switch (command[0]) {
-        case "push_front":
-          deque.push_front(Integer.parseInt(command[1]));
-          break;
-        case "push_back":
-          deque.push_back(Integer.parseInt(command[1]));
-          break;
-        case "pop_front":
-          sb.append(deque.pop_front()).append("\n");
-          break;
-        case "pop_back":
-          sb.append(deque.pop_back()).append("\n");
-          break;
-        case "size":
-          sb.append(deque.size()).append("\n");
-          break;
-        case "empty":
-          sb.append(deque.empty()).append("\n");
-          break;
-        case "front":
-          sb.append(deque.front()).append("\n");
-          break;
-        case "back":
-          sb.append(deque.back()).append("\n");
-          break;
-      }
+        System.out.print(sb.toString());
     }
-
-    System.out.print(sb.toString());
-  }
 }
