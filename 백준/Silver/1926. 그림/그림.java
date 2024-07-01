50fe5406eb668a02c62cb7b6fbd888b02dae2b49
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-07-02 03:04:09 +0900

    [Silver I] Title: 그림, Time: 404 ms, Memory: 50232 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -2,82 +2,77 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.function.IntFunction;
 
 public class Main {
 
-  static int n, m;
-  static int[][] graph;
-  static boolean[][] visited;
-  static int count = 0;
-  static int[][] pos = {{0, -1}, {0, 1}, {1, 0}, {-1, 0}};
-
-
-  static void bfs(int x, int y) {
-    visited[x][y] = true;
-    Queue<int[]> queue = new LinkedList<>();
-    queue.add(new int[]{x, y});
-
-    while (!queue.isEmpty()) {
-      int[] arr = queue.poll();
-      int nowX = arr[0];
-      int nowY = arr[1];
-
-      for (int i = 0; i < pos.length; i++) {
-        int nx = nowX + pos[i][0];
-        int ny = nowY + pos[i][1];
-
-        if (nx >= 0 && nx < n && ny >= 0 && ny < m && graph[nx][ny] == 1 && !visited[nx][ny]) {
-          count++;
-          visited[nx][ny] = true;
-          queue.add(new int[]{nx, ny});
-        }
-      }
-    }
-
-  }
+  static int n;
+  static int m;
+  static int[][] board;
+  static boolean[][] vis;
+  static int[] dx = {1, 0, -1, 0};
+  static int[] dy = {0, 1, 0, -1}; // 상하좌우 네 방향을 의미
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String[] input = br.readLine().split(" ");
     n = Integer.parseInt(input[0]);
     m = Integer.parseInt(input[1]);
-    boolean isZero = true;
-    List<Integer> answer = new ArrayList<>();
+    int biggestArea = 0;
+    int countOfArea = 0;
 
-    graph = new int[n][m];
-    visited = new boolean[n][m];
-
+    // board, vis 초기화
+    board = new int[n][m];
+    vis = new boolean[n][m];
     for (int i = 0; i < n; i++) {
       String[] inputArr = br.readLine().split(" ");
       for (int j = 0; j < m; j++) {
-        graph[i][j] = Integer.parseInt(inputArr[j]);
-        if (graph[i][j] == 1) {
-          isZero = false;
-        }
+        board[i][j] = Integer.parseInt(inputArr[j]);
       }
     }
 
-    if (isZero == true) {
-      System.out.println("0");
-      System.out.println("0");
-      return;
-    }
-
+    // 각 노드를 순회하면서 진행.
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        count = 0;
-        if (graph[i][j] == 1 && !visited[i][j]) {
-          count++;
-          bfs(i, j);
-          answer.add(count);
+        if (board[i][j] == 1 && vis[i][j] != true) {
+          biggestArea = Math.max(biggestArea, BFS(i, j));
+          countOfArea++;
         }
       }
     }
-    Collections.sort(answer);
-    System.out.println(answer.size());
-    System.out.println(answer.get(answer.size() - 1));
-
+    
+    System.out.println(countOfArea);
+    System.out.println(biggestArea);
+    
     br.close();
+  }
+
+  private static int BFS(int x, int y) {
+    // bfs 시작
+    Queue<int[]> Q = new LinkedList<>(); // 행렬을 그대로 넣어야 하므로 int[] 로 초기화
+    vis[x][y] = true;
+    Q.offer(new int[]{x, y}); // 큐에 시작점인 (x, y)을 삽입.
+    int count = 1;
+
+    while (!Q.isEmpty()) {
+      int[] cur = Q.poll(); // 현재 값 큐에서 꺼내서
+      int curX = cur[0];
+      int curY = cur[1];
+
+      for (int dir = 0; dir < 4; dir++) { // 상하좌우 칸을 살펴보기.
+        int nx = curX + dx[dir];
+        int ny = curY + dy[dir]; // nx, ny에 dir에서 정한 방향의 인접한 칸의 좌표가 들어감
+
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+          continue; // 범위 밖일 경우 넘어감
+        }
+        if (vis[nx][ny] || board[nx][ny] != 1) {
+          continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
+        }
+        count++;
+        vis[nx][ny] = true; // (nx, ny)를 방문했다고 명시
+        Q.offer(new int[]{nx, ny});
+      }
+    }
+    return count;
   }
 }
