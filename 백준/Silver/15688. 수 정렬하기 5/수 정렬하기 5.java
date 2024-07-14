ebe0a848046492aeeca2ea067e08c7875fb71eb9
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-07-14 20:46:04 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 15336 ms, Memory: 101380 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -10,19 +10,27 @@ public class Main {
     StringBuilder sb = new StringBuilder();
     int n = Integer.parseInt(br.readLine());
     int[] arr = new int[2000001]; // -1,000,000 ~ 1,000,000 범위
-    int offset = 1000000; // 배열 인덱스를 양수로 만들기 위해 오프셋 사용
+    int offset = 1000000; // 배열 인덱스를 양수로 만들기 위한 오프셋
+    int min = Integer.MAX_VALUE;
+    int max = Integer.MIN_VALUE;
 
+    // 입력 받기 및 범위 계산
     for (int i = 0; i < n; i++) {
       int cur = Integer.parseInt(br.readLine());
       arr[cur + offset] += 1;
+      if (cur < min) min = cur;
+      if (cur > max) max = cur;
     }
 
-    for (int i = 0; i < arr.length; i++) {
+    // 정렬된 결과 생성
+    for (int i = min + offset; i <= max + offset; i++) {
       while (arr[i] > 0) {
         sb.append(i - offset).append('\n');
         arr[i]--;
       }
     }
+
+    // 결과 출력
     System.out.print(sb.toString());
   }
 }
