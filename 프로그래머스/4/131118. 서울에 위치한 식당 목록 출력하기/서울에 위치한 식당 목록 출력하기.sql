5fc96206385ca557f49a586998ee84db29f21a8f
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-07-15 09:42:14 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,39 @@
+# # -- 리뷰의 평균 점수를 조회
+SELECT
+    I.REST_ID, -- REST_INFO, REST_REVIEW
+    I.REST_NAME, -- REST_INFO
+    I.FOOD_TYPE, --  REST_INFO
+    I.FAVORITES, --   REST_INFO
+    I.ADDRESS, -- REST_INFO
+    FORMAT(R.SCORE, 2) AS SCORE-- REST_REVIEW (리뷰의 평균 점수)
+    FROM
+        REST_INFO AS I
+        LEFT OUTER JOIN 
+            (SELECT
+            REST_ID,
+            AVG(REVIEW_SCORE) AS SCORE
+            FROM
+            REST_REVIEW
+            GROUP BY REST_ID) AS R
+    ON 
+        I.REST_ID = R.REST_ID
+    WHERE 
+        I.ADDRESS LIKE '서울%' 
+        AND 
+        I.REST_ID = R.REST_ID
+    ORDER BY 
+        SCORE DESC, 
+        FAVORITES DESC;
+
+
+# SELECT
+#             REST_ID,
+#             AVG(REVIEW_SCORE) AS SCORE
+#             FROM
+#             REST_REVIEW
+#             GROUP BY REST_ID
+
+
+        
+
+        
