aa2e20d10611c7ce91545108413cda8b774b1f46
JunR <68640939+lee-JunR@users.noreply.github.com>
2024-08-27 23:18:38 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.57 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+        int time = 0;
+        int idx = 0;
+        int len = jobs.length;
+
+        Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
+
+        Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
+
+        while (!pq.isEmpty() || idx < len) {
+            while (idx < len && jobs[idx][0] <= time) {
+                pq.offer(jobs[idx++]);
+            }
+
+            if (pq.isEmpty()) {
+                time = jobs[idx][0];
+            }
+
+            else {
+                int[] job = pq.poll();
+                time += job[1];
+                answer += time - job[0];
+            }            
+        }
+
+        return answer / len;
+    }
+}
